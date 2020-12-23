7896a238d8a26565e004b7b20a808caef23e2b79
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-23 21:21:09 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,2 +1,7 @@
-# Write your solution herecurrent_time = Time.now/nif(current_time.to_s % 2) == 0\n\tputs "Even!"\nelse\n\tputs "Odd!"\nend
-\ncurrent_time = Time.now/nif(current_time.to_s % 2) == 0\n\tputs "Even!"\nelse\n\tputs "Odd!"\nend
+# Write your solution here
+current_time = Time.now
+if(current_time.to_s % 2) == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
